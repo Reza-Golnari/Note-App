464586ac73aa4bd890e0b06ef60af60eca6b8d3b
Reza <i.am.reza.programmer@gmail.com>
2023-10-19 18:21:31 +0330

    create app

---
+++ composables/states.ts
@@ -0,0 +1,4 @@
+// export const useCounter = () => useState<number>("counter", () => 0);
+// export const useColor = () => useState<string>("color", () => "pink");
+
+// export const useName = () => useState<string>("name", () => "Reza");
